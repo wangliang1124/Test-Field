db03160bd52fc171925ded42ac273d0eb5d5570c
LeonWang1124 <wangliang1124@163.com>
2017-04-10 17:08:06 +0800

    添加键盘控制

--- exercise1/keyboardEvent/js/script.js
+++ exercise1/keyboardEvent/js/script.js
@@ -1,12 +1,38 @@
-window.onload=function(){	
+window.onload=function(){
 	var start=document.getElementById('start');
 	var title=document.getElementById('title');
-	start.onclick=function(){
-		var elems=['测试1','测试2','测试3'];
-		setInterval(function(){
-			var r=Math.floor(Math.random()*3);
-			title.innerHTML=elems[r];
-		},50)
+	var timer=null;
+	var status=true;
 
+	//鼠标动作
+	start.onclick=play;
+	//键盘控制
+	document.onkeyup=function(event){
+		event=event || window.event;
+		//console.log(event.keyCode);
+		if(event.keyCode==32){
+			play();
+		}	
 	}
+
+	//
+	function play(){
+		if(status){
+			var elems=['IPhone7','华为Mate10','小米MIX','红米NOTE','魅族5','锤子T2','小米5','充值卡100元','谢谢参与'];
+			timer=setInterval(function(){
+				var r=Math.floor(Math.random()*elems.length);
+				title.innerHTML=elems[r];
+			},50)
+			start.innerHTML='停止';
+			status=false;
+		}else{
+			clearInterval(timer);
+			status=true;
+			start.innerHTML='开始';
+		}
+	}
+
+
 }
+
+
