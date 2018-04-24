28f6784c94665103c70114438923d79756d68279
anotherleon <wangliang1124@163.com>
2018-04-24 22:35:44 +0800

    全屏滚动

--- 16.FE-interview/pageSlide/pageSlide.js
+++ 16.FE-interview/pageSlide/pageSlide.js
@@ -7,11 +7,31 @@ const pageSlide = (function(){
   }
   var eventHandler = {
     wheel: function(e) {
+      console.log(this.isSliding)
+      if(this.isSliding) return
       e = e || window.event
       if(e.wheelDelta < 0 || e.detail > 0) {
+        this.isSliding = true
         this.next()
       } else {
         this.prev()
+      }
+    },
+    key: function(e) {
+      if(this.isSliding) return
+      e = e || window.event
+      if(e.key === 'ArrowDown') { // keyCode = 40
+        this.next()
+      }
+      if(e.key === 'ArrowUp') { // keyCode = 38
+        this.prev()
+      }
+    },
+    transition: function(e) {
+      e = e || window.event
+      console.log(e)
+      if(e.target === this.pages[this.index]) {
+        this.isSliding = false
       }
     }
   }
@@ -28,23 +48,27 @@ const pageSlide = (function(){
     off: function() {},
   }
   //绑定事件
-  var bindEvent = function(obj) {
-    EventUtils.on(document, 'mousewheel', eventHandler.wheel.bind(obj))
-    EventUtils.on(document, 'DOMMouseScroll', eventHandler.wheel.bind(obj))
+  var bindEvent = function() {
+    EventUtils.on(document, 'mousewheel', eventHandler.wheel.bind(this))
+    EventUtils.on(document, 'DOMMouseScroll', eventHandler.wheel.bind(this))
+    EventUtils.on(document, 'keyup', eventHandler.key.bind(this))
+    EventUtils.on(this.slide, 'transitionend', eventHandler.transition.bind(this))
   } 
   // 初始化
-  var initTransition = function(els, index) {
-    for(var i = 0, el = els[i]; i < els.length; i++){
-      if (i === index) el.style.transform = 'translate3d(0, 0, 0)'
-    }
-  }
+  // var initTransition = function(els, index) {
+  //   for(var i = 0, el = els[i]; i < els.length; i++){
+  //     if (i === index) el.style.transform = 'translate3d(0, 0, 0)'
+  //   }
+  // }
   // 构造函数
   function pageSlide(options) {
     this.opts = Object.assign({}, defaults, options)
     this.slide = document.querySelector(this.opts.slide)
     this.pages = document.querySelectorAll(this.opts.pages)
     this.count = this.pages.length
+    this.lastIndex = 0
     this.index = 0
+    this.isSliding = false
     this.init(this.index)
     if(this.opts.transition){ // && initTransition(this.pages, this.index)
       for(var i = 0; i < this.count; i++) {
@@ -59,7 +83,7 @@ const pageSlide = (function(){
       }
     } 
     // 绑定事件监听
-    bindEvent(this)
+    bindEvent.call(this)
   }
   pageSlide.prototype.init = function(index) {
     index > this.count - 1 ? index = this.count - 1 : index
@@ -82,6 +106,7 @@ const pageSlide = (function(){
     this.pages[this.index - 1].style.transform = 'translate3d(0, 0, 0)'
     this.index--
   }
+  // pageSlide.prototype.slideTo = function(index) {}
   pageSlide.prototype.start = function() {}
   pageSlide.prototype.stop = function() {}
 
