bb276b1e279ca73fd549185ac4a32e485166f683
anotherleon <wangliang1124@163.com>
2018-07-28 20:49:11 +0800

    add小程序demo项目

---
+++ my-project-mp/src/pages/index/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import App from './index';
+
+// import elAlert from './../../components/alert/index';
+
+// Vue.component(elAlert.name, elAlert);
+// Vue.use(elAlert)
+// elAlert.install(Vue)
+
+const app = new Vue(App);
+app.$mount();
